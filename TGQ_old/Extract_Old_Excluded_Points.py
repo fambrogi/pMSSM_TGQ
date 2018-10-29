5b044b4c3311ba096cc149eea8baf62553d5ba96
Federico Ambrogi <fambrogi@cern.ch>
2018-10-29 10:00:52 +0100

    Checked old list excluded points

--- TGQ_old/Extract_Old_Excluded_Points.py
+++ TGQ_old/Extract_Old_Excluded_Points.py
@@ -1,40 +1,15 @@
-# Extract the masses from the list of excluded points
-
-
-'''
 a = open('SummaryBino_noHeavyHiggsExclusion.txt','r').readlines() 
 
-out = open('Bino_old_excluded','w')
+out = open('Bino_old_excluded','r')
 out.write('#Old Bino Exclusion')
 
 for l in a[1:]:
  splitt = l.split(' ')
  slha = splitt[0]
- rUL = float(splitt[58])
- rEM = float(splitt[61])
+ rUL = splitt[58]
+ rEM = splitt[61]
  if rUL > 1.0001 or rEM > 1.0001:
         out.write(slha + '\n')
-#        print 'Excluded!', rUL , ' ' , rEM
-# else: print 'Not Excluded'
 
 out.close()
-'''
-
-a = open('SummaryHiggsino_noHeavyHiggsExclusion.txt','r').readlines()
-
-out = open('Higgsino_old_excluded','w')
-out.write('#Old Higgsino Exclusion')
-
-for l in a[1:]:
- splitt = l.split(' ')
- slha = splitt[0]
- rUL = float(splitt[58])
- rEM = float(splitt[61])
- if rUL > 1.0001 or rEM > 1.0001:
-        out.write(slha + '\n')
-#        print 'Excluded!', rUL , ' ' , rEM
-# else: print 'Not Excluded'
-out.close()
-
-
 
